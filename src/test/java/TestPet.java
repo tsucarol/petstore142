cd1958b3eab3ba23219152478c934db798645443
tsucarol <carollyne.guim@gmail.com>
2025-02-20 21:49:55 -0300

    first commit

---
+++ src/test/java/TestPet.java
@@ -0,0 +1,30 @@
+/* 0 - Nome do pacote */ 
+
+/* 1 - Bibliotecas */ 
+import org.junit.jupiter.api.Test;
+
+/* 2 - Classe */
+
+public class TestPet {
+    /* 2.1 - Atributos */ 
+    static String ct = "application/json"; // CT = Content Type
+    static String uriUser = "https://petstore.swagger.io/v2/user";
+
+    /* 2.2 - Funções e Métodos */ 
+    
+    /* 2.2.1 funções e métodos comuns / úteis */
+    public static String lerArquivoJson(String arquivoJson){ // Função de leitura de Json
+        // TODO: Completar a leitura do arquivo
+        //return new String(Files);
+        return null;
+    }
+
+
+    /* 2.2.2 métodos de teste */
+    @Test
+    public void testPostPet() {
+        /* Carregar os dados do arquivo json do pet */
+        //TODO: Criar o método POST
+    }
+
+}
